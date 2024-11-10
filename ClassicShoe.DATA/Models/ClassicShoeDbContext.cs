8b6ceaf16a00364e673e6c6f92df4fa91a165d57
HungHe198 <levanhung.1982004@gmail.com>
2024-11-11 01:29:42 +0700

    update database and add nuget

---
+++ ClassicShoe.DATA/Models/ClassicShoeDbContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassicShoe.DATA.Models
+{
+    public class ClassicShoeDbContext : DbContext
+    {
+        public ClassicShoeDbContext()
+        {
+        }
+        public ClassicShoeDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+       
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            base.ConfigureConventions(configurationBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=LEVANHUNG\\LEVANHUNG;Initial Catalog=ClassicShoe;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<DeGiay> DeGiays { get; set; }
+        public DbSet<Giay> Giays { get; set; }
+        public DbSet<GiayChiTiet> GiayChiTiets { get; set; }
+        public DbSet<HangSanXuat> HangSanXuats { get; set; }
+        public DbSet<HoaDon> HoaDons { get; set; }
+        public DbSet<HoaDonChiTiet> HoaDonChiTiets { get; set; }
+        public DbSet<KhachHang> KhachHangs { get; set; }
+        public DbSet<LoaiGiay> LoaiGiays { get; set; }
+        public DbSet<MaGiamGia> MaGiamGias { get; set; }
+        public DbSet<MauSac> MauSacs { get; set; }
+        public DbSet<NhanVien> NhanViens { get; set; }
+        public DbSet<ThanGiay> ThanGiays { get; set; }
+        public DbSet<VaiTro> VaiTros { get; set; }
+    }
+}
